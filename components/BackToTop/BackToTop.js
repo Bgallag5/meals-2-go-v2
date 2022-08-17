4e8baf2be5391c916d91d4808c4983ded2f8bf55
bengallagher <bgallag5@gmail.com>
2022-08-17 14:21:52 -0400

    BTT pulse

--- components/BackToTop/BackToTop.js
+++ components/BackToTop/BackToTop.js
@@ -9,15 +9,24 @@ export default function BackToTop() {
   const topRef = useRef();
 
   const scrollToTop = () => {
+    topRef.current.classList.remove(styles.pulse)
+    void topRef.current.offsetWidth;
+    topRef.current.classList.add(styles.pulse)
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
+//   useEffect(() => {
+//     topRef.current.onClick = () => {
+//         topRef.current.classList.add(styles.pulse)
+//     }
+//   }, [])
 
   //if scrollY > 850 show back to top button
   useEffect(() => {
     window.onscroll = function () {
       if (window.scrollY >= 850) {
         topRef.current?.classList.add(`${styles.visible}`);
-      } else {
+      } else if(window.scrollY <= 100) {
         topRef.current?.classList.remove(`${styles.visible}`);
       }
     };
